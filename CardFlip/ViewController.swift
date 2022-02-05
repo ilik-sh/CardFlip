5111ba45c2b690fbcef73d39b9881bcd69d954cc
ilik_sh <i.shyshparonak@gmail.com>
2022-02-05 18:30:36 +0300

    Initial commit

---
+++ CardFlip/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CardFlip
+//
+//  Created by Ilya on 5.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
