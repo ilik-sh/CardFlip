cc29c577b48a6db6465fb0e90cb358e21a441215
ilik_sh <i.shyshparonak@gmail.com>
2022-03-04 18:45:49 +0300

    Add extension for UIImage resizing

---
+++ CardFlip/Extensions/UIImageExtension.swift
@@ -0,0 +1,19 @@
+//
+//  UIImageExtension.swift
+//  CardFlip
+//
+//  Created by Ilya on 4.03.22.
+//
+
+import Foundation
+import UIKit
+
+extension UIImage {
+    func resize( size: CGSize) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
+        self.draw(in: CGRect(origin: CGPoint.zero, size: size))
+        let resizedImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        return resizedImage
+    }
+}
